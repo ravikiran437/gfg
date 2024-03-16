0bd356f2485e26a9d717a78cebd804f38b4f53f8
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2024-03-16 23:19:31 +0530

    Updated Solution - GfG to GitHub

--- Easy/Delete without head pointer/delete-without-head-pointer.py
+++ Easy/Delete without head pointer/delete-without-head-pointer.py
@@ -1,24 +1,29 @@
 #User function Template for python3
-'''Your task is to delete the given node from
+'''
+    Your task is to delete the given node from
 	the linked list, without using head pointer.
 	
 	Function Arguments: node (given node to be deleted) 
-	Return Type: None, just delete the given node from the linked list.'''
-		# Node Class
-class Node:
-    def __init__(self, data):   # data -> value stored in node
-        self.data = data
-        self.next = None
+	Return Type: None, just delete the given node from the linked list.
 
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+'''
 class Solution:
     #Function to delete a node without any reference to head pointer.
     def deleteNode(self,del_node):
-        #code here
+        #code here\
         if not del_node.next :
             del_node.data = None 
         del_node.data = del_node.next.data 
         del_node.next = del_node.next.next
-
+        
+        
 
 #{ 
  # Driver Code Starts
