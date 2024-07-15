e6cdf09721fa4959fb5fb67c4fc219528d8ff6bb
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2024-07-15 10:16:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Remove K Digits/remove-k-digits.py
+++ Difficulty: Medium/Remove K Digits/remove-k-digits.py
@@ -1,25 +1,29 @@
 #User function Template for python3
 
 class Solution:
-    def removeKdigits(self, s: str, k: int) -> str:
-        if len(s) <= k:
-            return "0"
 
+    def removeKdigits(self, num, k):
         stack = []
-        for digit in s:
-            while k > 0 and stack and stack[-1] > digit:
+        for i in num:
+            while k and stack and stack[-1]>i :
                 stack.pop()
-                k -= 1
-            stack.append(digit)
-
-        # If there are still digits to remove, remove from the end
-        stack = stack[:-k] if k > 0 else stack
-
-        # Convert the stack back to a string and strip leading zeros
-        result = "".join(stack).lstrip('0')
+                k -= 1 
+            stack.append(i)
+        if k !=0:
+            stack = stack[:-k]
+        if stack == []:
+            return "0"
+        p = "".join(stack)
+        p = p.lstrip("0")
+        if p == "":
+            return "0"
+        return p 
+            
         
-        # If the result is an empty string, return "0"
-        return result if result else "0"
+            
+        
+
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
