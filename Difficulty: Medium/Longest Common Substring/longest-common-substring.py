ae25c55d3120c2b07172c466eeb7812e567f780d
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2024-08-14 04:01:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    def longestCommonSubstr(self, str1, str2):
+        # code here
+        s = ""
+        m = 0 
+        for i in str1:
+            if i in str2:
+                s += i 
+                if s in str2:
+                    m = max(m,len(s))
+                else:
+                    s = i 
+            else:
+                s = ""
+        return m
+                
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        S1 = input().strip()
+        S2 = input().strip()
+        ob = Solution()
+        print(ob.longestCommonSubstr(S1, S2))
+
+# } Driver Code Ends
