39a883eddc6ed450214e3e5b0734bab5737c4df9
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2024-09-09 21:14:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.py
@@ -0,0 +1,45 @@
+import bisect
+class Solution:
+    #User function Template for python3
+    
+    # arr[]: Input Array
+    # N : Size of the Array arr[]
+    #Function to count inversions in the array.
+    def inversionCount(self, l):
+        # Your Code Here
+        k = []
+        c = 0 
+        for  i in l[::-1] :
+            p = bisect.bisect_left(k,i)
+            c += p
+            k.insert(p,i)
+        return c
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for tt in range(t):
+        a = list(map(int, input().strip().split()))
+        obj = Solution()
+        print(obj.inversionCount(a))
+
+# } Driver Code Ends
