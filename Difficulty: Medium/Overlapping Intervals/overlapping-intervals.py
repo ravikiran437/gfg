e2af3c06c399925361a87148486f009561ac25ed
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2024-12-08 09:25:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.py
@@ -1,46 +1,47 @@
 class Solution:
-	def overlappedInterval(self, arr):
+	def mergeOverlap(self, arr):
 		#Code here
 		a,b = [],[]
-		for  i,j in arr:
-		    a.append(i)
-		    b.append(j)
-		a.sort()
-		b.sort()
-		c = a[0]
-		d = b[0]
-		res = []
-		for i in range(1,len(a)):
-		    if d >= a[i]:
-		        d = b[i]
-		    elif d < a[i]:
-		        res.append([c,d])
-		        c = a[i]
-		        d = b[i]
-		res.append([c,d])
-		return res
-		        
+        for  i,j in arr:
+            a.append(i)
+            b.append(j)
+        a.sort()
+        b.sort()
+        c = a[0]
+        d = b[0]
+        res = []
+        for i in range(1,len(a)):
+            if d >= a[i]:
+                d = b[i]
+            elif d < a[i]:
+                res.append([c,d])
+                c = a[i]
+                d = b[i]
+        res.append([c,d])
+        return res
+
 
 #{ 
  # Driver Code Starts
-		
 if __name__ == '__main__':
-    T=int(input())
+    T = int(input())
     for i in range(T):
-    	n = int(input())
-    	a = list(map(int, input().strip().split()))
-    	Intervals = []
-    	j = 0
-    	for i in range(n):
-    		x = a[j]
-    		j += 1
-    		y = a[j]
-    		j += 1
-    		Intervals.append([x,y])
-    	obj = Solution()
-    	ans = obj.overlappedInterval(Intervals)
-    	for i in ans:
-    		for j in i:
-    			print(j, end = " ")
-    	print()
+        n = int(input())
+        # a = list(map(int, input().strip().split()))
+        arr = []
+        # j = 0
+        for i in range(n):
+            a = list(map(int, input().strip().split()))
+            x = a[0]
+            # j += 1
+            y = a[1]
+            # j += 1
+            arr.append([x, y])
+        obj = Solution()
+        ans = obj.mergeOverlap(arr)
+        for i in ans:
+            for j in i:
+                print(j, end=" ")
+        print()
+
 # } Driver Code Ends
