c78bb1b68df51452fcbc08413b456a493a3eb857
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2025-01-02 19:23:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
+++ Difficulty: Medium/Subarrays with sum K/subarrays-with-sum-k.py
@@ -1,16 +1,16 @@
 #User function Template for python3
 
 class Solution:
-    def findSubArraySum(self, Arr, N, k):
-        # code here
+    def countSubarrays(self, arr, k):
+        # code here\
         d = {}
         cnt = 0 
         s = 0 
-        for i in range(N):
-            s += Arr[i]
+        for i in range(len(arr)):
+            s += arr[i]
             if s == k:
                 cnt += 1 
-            rem = s-k 
+            rem = s - k 
             if rem in d:
                 cnt += d[rem]
             if s not in d:
@@ -20,18 +20,37 @@ class Solution:
         return cnt
 
 
+
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
+import atexit
+import io
+import sys
+from collections import deque
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
 if __name__ == '__main__':
-    t = int(input())
-    for _ in range (t):
-        N = int(input())
-        Arr = input().split()
-        for itr in range(N):
-            Arr[itr] = int(Arr[itr])
+    test_cases = int(input())
+    for cases in range(test_cases):
         k = int(input())
+        arr = list(map(int, input().strip().split()))
         ob = Solution()
-        print(ob.findSubArraySum(Arr, N, k))
+        res = ob.countSubarrays(arr, k)
+        print(res)
+        print("~")
+
 # } Driver Code Ends
