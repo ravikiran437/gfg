9b12840be205ffa78a594d29066a96e0112ead94
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2025-03-06 20:43:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Node at a given index in linked list/node-at-a-given-index-in-linked-list.py
@@ -0,0 +1,61 @@
+"""index is the node which is to be displayed in output
+  Node is defined as
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+# Linked List class contains a Node object
+class LinkedList:
+    def __init__(self):
+        self.head = None
+This is method only submission.
+ You only need to complete below method.
+"""
+class Solution:
+    def GetNth(self, head, index):
+        # Code Here
+        c = 1 
+        while head:
+            if index == c:
+                return head.data
+            head = head.next 
+            c += 1
+        return -1
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def print_list(node):
+    while node:
+        print(node.data, end=' ')
+        node = node.next
+    print()
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split('\n')
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        arr = list(map(int, data[index].split()))
+        k = int(data[index + 1])
+        head = Node(arr[0])
+        tail = head
+        for value in arr[1:]:
+            tail.next = Node(value)
+            tail = tail.next
+        ob = Solution()
+        res = ob.GetNth(head, k)
+        print(res)
+        print("~")
+        index += 2
+
+# } Driver Code Ends
