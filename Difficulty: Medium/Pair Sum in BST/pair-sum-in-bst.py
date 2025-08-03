0cb116185ec2dd8bc11b6902d17de7f2635b828b
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2025-08-03 19:18:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pair Sum in BST/pair-sum-in-bst.py
+++ Difficulty: Medium/Pair Sum in BST/pair-sum-in-bst.py
@@ -1,28 +1,23 @@
-'''
-# Tree Node
-class Node:
-    def __init__(self, val):
-        self.right = None
-        self.data = val
-        self.left = None
+#User function Template for python3
 
-'''
 
-class Solution:
-    def findTarget(self, root, target): 
-        # your code here.
-        stack = [root]
-        d = {}
-        while stack:
-            node = stack.pop()
-            dif = target - node.data
-            if dif in d:
-                return True 
-            if node.data not in d:
-                d[node.data] = 1
+#Function to check if any pair exists in BST whose sum is equal to given value.
+def findPair(root,X):
+    # code here    
     
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-        return False
+    # code here    
+
+    stack = [root]
+    d = {}
+    while stack:
+        node = stack.pop()
+        diff = X - node.key
+        if diff in d:
+            return True
+        if node.key not in d:
+            d[node.key] = 1
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+    return False
