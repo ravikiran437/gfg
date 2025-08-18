bb702d84b918ee94bb01c6cf62f5369c5be6ce93
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2025-08-18 18:19:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Pattern Search/pattern-search.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if the given pattern exists in the given string or not.
+    def search(self,p,s):
+        #code here
+        if p in s:
+            return True
+        return False
