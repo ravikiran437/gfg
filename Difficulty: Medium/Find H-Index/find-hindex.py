4707af54b03ecfe9284d4f848bc5768d9eeb8672
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2025-08-18 10:49:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hIndex(self, cit):
+        cit.sort()
+        n  = len(cit)
+        for i in range(n):
+            if cit[i] >= (n-i):
+                return n-i
+        return 0
+      
