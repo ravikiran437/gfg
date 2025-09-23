8e1981bcefd2684613ca2249c62cc75c18e31216
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2025-09-23 18:31:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Queue Reversal/queue-reversal.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseQueue(self, q):
+        # code here
+        l,r = 0 ,len(q)-1
+        while l < r:
+            q[l],q[r] = q[r],q[l]
+            l += 1 
+            r -= 1 
+        return q
+            
