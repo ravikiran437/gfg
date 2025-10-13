a8eb79b2eb965899d5259f4d59635c15e2bc5440
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2025-10-13 09:15:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.py
@@ -0,0 +1,16 @@
+
+import heapq
+class Solution:
+   def minCost(self, arr):
+       
+       heapq.heapify(arr)
+       total_cost = 0 
+       while len(arr) > 1 :
+           a = heapq.heappop(arr)
+           b = heapq.heappop(arr)
+           c = a + b 
+           total_cost += c 
+           heapq.heappush(arr,c)
+       return total_cost
+        
+           
