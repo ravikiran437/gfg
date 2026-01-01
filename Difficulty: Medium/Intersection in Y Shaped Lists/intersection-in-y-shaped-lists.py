083aee346a404696242104e9ebfad2b5aeaf2e96
RAVI KIRAN <155363112+ravikiran437@users.noreply.github.com>
2026-01-01 11:25:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.py
+++ Difficulty: Medium/Intersection in Y Shaped Lists/intersection-in-y-shaped-lists.py
@@ -9,12 +9,13 @@ class Node:
 class Solution:
     def intersectPoint(self, head1, head2):
         # code here
-        nodes = set()
+        nodes  = set()
         while head1:
-            nodes.add(id(head1))
-            head1 = head1.next 
+            nodes.add(head1)
+            head1  = head1.next 
         while head2:
-            if id(head2) in nodes:
+            if head2 in nodes:
                 return head2 
             head2 = head2.next 
         return 0
+        
